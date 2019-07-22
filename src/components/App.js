a6d51d4ec4d82613563bd9a810b00e6db946ddfd
ashu961 <beheraashutosh5@gmail.com>
2019-07-23 00:35:21 +0530

    nav update

--- src/components/App.js
+++ src/components/App.js
@@ -19,44 +19,42 @@ class App extends React.Component {
         return (
             <div>
                 <div>
-                <div class="ui inverted menu">
-                    <div class="header item">Brand</div>
-                    <div class="active item">Link</div>
-                    <a class="item">Link</a>
-                    <div class="ui dropdown item">
-                        Dropdown
-                     <i class="dropdown icon"></i>
-                        <div class="menu">
-                            <div class="item">Action</div>
-                            <div class="item">Another Action</div>
-                            <div class="item">Something else here</div>
-                            <div class="divider"></div>
-                            <div class="item">Separated Link</div>
-                            <div class="divider"></div>
-                            <div class="item">One more separated link</div>
-                        </div>
+                    <div class="ui inverted menu">
                     </div>
-                    <div>
-                        <div class="item">
-                            <div class="ui transparent inverted icon input">
+                    <a class="item"><div><GoogleAuth /></div></a>
+                </div>
+                <div class="item">
+                        <div class="ui transparent inverted icon input">
                             <div><SearchBar oniSubmit={this.oniSubmit} /></div>
-                             </div>
                         </div>
-                        </div>
-                            <a class="item"><div><GoogleAuth /></div></a>
-                        </div>
+                <a class="item">Link</a>
+                <div class="ui dropdown item">
+                    Dropdown
+                     <i class="dropdown icon"></i>
+                    <div class="menu">
+                        <div class="item">Action</div>
+                        <div class="item">Another Action</div>
+                        <div class="item">Something else here</div>
+                        <div class="divider"></div>
+                        <div class="item">Separated Link</div>
+                        <div class="divider"></div>
+                        <div class="item">One more separated link</div>
                     </div>
-                    <div class="ui column grid">
-                        <div class="column">
-                            <div class="ui segment">
-                                <ImageList images={this.state.images} />
-                            </div>
+                </div>
+                <div> 
+                    </div>
+                </div>
+                <div class="ui column grid">
+                    <div class="column">
+                        <div class="ui segment">
+                            <ImageList images={this.state.images} />
                         </div>
                     </div>
                 </div>
-                );
-            }
-        }
-        export default App;
+            </div>
+        );
+    }
+}
+export default App;
 
 
